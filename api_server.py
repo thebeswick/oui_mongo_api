3a54f96dd4269a81824ea406862af20f3e58639b
Rob_Beswick <rjbeswick@gmail.com>
2017-09-18 17:29:25 +1000

    initial release

---
+++ api_server.py
@@ -0,0 +1,41 @@
+# Small Flask program to pull OUI (Original Unique Identifier) data
+# There is an accompanying script that needs to be executed first
+# to download the IEEE OUI text file and import it into MongoDB
+
+import re
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'wifidb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/wifidb'
+
+mongo = PyMongo(app)
+
+@app.route('/api/all', methods=['GET'])
+def get_all_mac_addresses():
+    macaddr = mongo.db.ouidata
+    output = []
+    for macquery in macaddr.find():
+        output.append({"vendorOUI" : macquery['vendorOUI'], "VendorName" : macquery['vendorName'] })
+    return jsonify({'result' : output})
+
+@app.route('/api/<macaddress>', methods=['GET'])
+def get_one_mac(macaddress=None):
+    macaddr = mongo.db.ouidata
+    macAddr = re.sub('[:-]', '', macaddress)
+    vendorMACPrefix = macAddr[0:6].upper()
+    print vendorMACPrefix
+    macquery = macaddr.find_one({"vendorOUI": vendorMACPrefix})
+    output = []
+    if macquery:
+        output.append({"vendorOUI" : macquery['vendorOUI'], "VendorName" : macquery['vendorName'] })
+    else:
+      output = "No such name"
+    return jsonify({'result' : output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ oui_data_import.py
@@ -0,0 +1,45 @@
+# This is a script to download the IEEE OUI text file and import it into MongoDB
+# If you already have a copy of the oui.txt file you can comment out get_mac_table_file()
+# to save time. The script is dependent on MongoDB
+
+import os
+import sys
+import urllib2
+import json
+import re
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.wifidb
+ven_arr = []
+
+
+def get_mac_table_file(filename="oui.txt"):
+    request = urllib2.urlopen("http://standards.ieee.org/develop/regauth/oui/oui.txt")
+    with open(filename, "w") as f:
+        for line in request:
+            f.write(line)
+
+
+def import_oui_data():
+    db.ouidata.drop()
+    filename = "./oui.txt"
+    with open(filename, "r") as f:
+        for line in f:
+            if "(base 16)" not in line:
+                continue
+            ven = tuple(re.sub("\s*([0-9a-zA-Z]+)[\s\t]*\(base 16\)[\s\t]*(.*)\n", r"\1;;\2", line).split(";;"))
+            ven_arr.append(ven)
+    arr_length = len(ven_arr)
+    pos = 0
+    while pos < len(ven_arr):
+        vendorPrefixMAC = ven_arr[pos][0]
+        vendorName = ven_arr[pos][1].rstrip()
+        db.ouidata.insert({"vendorMAC": vendorPrefixMAC, "vendorName": vendorName})
+        pos += 1
+
+
+if __name__ == '__main__':
+#    get_mac_table_file()
+    import_oui_data()
